a7c606d1d4073fbbab83b5ddff8cc86839a63f9a
Arthur <arthurvms10@gmail.com>
2025-03-25 09:14:11 -0300

    feat: updated PORT in code for 5000

--- serverTelegram.py
+++ serverTelegram.py
@@ -100,4 +100,5 @@ def notify_gas_level():
         return jsonify({"gas_levels": gas_levels, "count": len(gas_levels)}), 200
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=10000)
+    PORT = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=PORT)
